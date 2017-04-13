0114b0be3862c3f7a88c3720b8569c839baf7eb2
Thais Diniz <thais.diniz@miningmath.com>
2017-04-13 20:28:05 -0300

    working

--- principal.js
+++ principal.js
@@ -11,4 +11,22 @@
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+let button = document.getElementById('rolar');
+let dices = document.getElementsByClassName('dado');
+let result = document.getElementById('recipienteResultados');
 
+button.addEventListener('click', compute);
+
+function compute(){
+    if (result.classList.contains('oculto')){
+        result.classList.remove('oculto');
+    }
+    for (let dice of dices){
+        let rand = Math.ceil(Math.random() * Number(dice.children[1].max));
+        let finalNumber = Number(dice.children[1].value) * rand; 
+        result.innerHTML += dice.children[1].value + ' * ' + rand + ' = ' +  finalNumber + '<br>';
+        
+        //result.innerHTML += value;
+    }
+
+}
